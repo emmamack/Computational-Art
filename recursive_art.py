3c234433c2712c659afae86e490b4e506db6c637
Emma Mack <emack@olin.edu>
2019-02-24 11:55:21 -0500

    adding code, 2 images, and reflection

--- recursive_art.py
+++ recursive_art.py
@@ -1,19 +1,54 @@
 """
-YOUR HEADER COMMENT HERE
+Create recursive art
 
-@author: YOUR NAME HERE
+@author: Emma Mack
 """
 
 import random
+import math
 from PIL import Image
 
+#I attempted to "go beyond" and got pretty far, but eventually ran into a wall.
+#When generate_art runs, it does not use random_f_lambda.
+def random_f_lambda(min_depth, max_depth, current_depth = 1):
+    """Builds a random function of x and y of depth at least min_depth and depth
+    at most max_depth.
 
-def build_random_function(min_depth, max_depth):
-    """Build a random function.
+    Args:
+        min_depth: the minimum depth of the random function
+        max_depth: the maximum depth of the random function
 
-    Builds a random function of depth at least min_depth and depth at most
-    max_depth. (See the assignment write-up for the definition of depth
-    in this context)
+    Returns:
+        Float
+
+    >>> random_f_lambda(6,8,.5,.7)
+
+    """
+    #if at or above min depth, certain probablilty that recursion will end
+    if current_depth >= min_depth:
+        #probability is inversely proportional to dist between min and max depth
+        if random.randint(min_depth, max_depth) == min_depth:
+            return random.choice([lambda x,y: x, lambda x,y: y])
+    if current_depth == max_depth:
+        return random.choice([lambda x,y: x, lambda x,y: y])
+
+    # func_choices = ["prod", "avg", "cos_pi", "sin_pi", "square", "round"]
+    func_choices = ["cos_pi", "prod"]
+    choice = random.choice(func_choices)
+
+
+    a = random_f_lambda(min_depth, max_depth, current_depth+1)
+    b = random_f_lambda(min_depth, max_depth, current_depth+1)
+
+    if choice == "cos_pi":
+        func = lambda x, y: math.cos(math.pi*a)
+    if choice == "prod":
+        func = lambda x, y: a*b
+    return func
+
+def random_f(min_depth, max_depth, current_depth = 1):
+    """Builds a random function of depth at least min_depth and depth at most
+    max_depth.
 
     Args:
         min_depth: the minimum depth of the random function
@@ -21,14 +56,32 @@ def build_random_function(min_depth, max_depth):
 
     Returns:
         The randomly generated function represented as a nested list.
-        (See the assignment writ-eup for details on the representation of
-        these functions)
+
+    >>> random_f(4,1)
+
     """
-    # TODO: implement this
-    pass
+    #if at or above min depth, certain probablilty that recursion will end
+    if current_depth >= min_depth:
+        #probability is inversely proportional to dist between min and max depth
+        if random.randint(min_depth, max_depth) == min_depth:
+            return random.choice([["x"],["y"]])
+    if current_depth == max_depth:
+        return random.choice([["x"],["y"]])
+
+    #randomly selects a func
+    func_choices = ["prod", "avg", "cos_pi", "sin_pi", "square", "round"]
+    choice = random.choice(func_choices)
+
+    #broken into functions that create two new recursive trees and those that create one
+    if choice == "cos_pi" or choice == "sin_pi" or choice == "square" or choice == "round":      #funcs with 1 arg
+        func = [choice, random_f(min_depth, max_depth, current_depth+1)]
+    if choice == "prod" or choice == "avg":           #funcs with 2 args
+        func = [choice, random_f(min_depth, max_depth, current_depth+1),
+                        random_f(min_depth, max_depth, current_depth+1)]
+    return func
 
 
-def evaluate_random_function(f, x, y):
+def eval_f(f, x, y):
     """Evaluate the random function f with inputs x,y.
 
     The representation of the function f is defined in the assignment write-up.
@@ -42,13 +95,34 @@ def evaluate_random_function(f, x, y):
         The function value
 
     Examples:
-        >>> evaluate_random_function(["x"],-0.5, 0.75)
-        -0.5
-        >>> evaluate_random_function(["y"],0.1,0.02)
-        0.02
+        >>> eval_f(["cos_pi",["prod",["x"],["y"]]],1, 2)
+        3.0
     """
-    # TODO: implement this
-    pass
+    operation = f[0]
+
+    #base case
+    if operation == "x":
+        return x
+    if operation == "y":
+        return y
+
+    #recursively evaluate function based on operation
+    if operation == "prod":
+        mult1, mult2 = f[1], f[2]
+        return eval_f(mult1,x,y)*eval_f(mult2,x,y)
+    if operation == "avg":
+        return (eval_f(f[1],x,y)+eval_f(f[2],x,y))/2
+    if operation == "cos_pi":
+        return math.cos(math.pi*eval_f(f[1],x,y))
+    if operation == "sin_pi":
+        return math.sin(math.pi*eval_f(f[1],x,y))
+    if operation == "square":
+        return eval_f(f[1],x,y)**2
+    if operation == "round":
+        return round(eval_f(f[1],x,y))
+    else:
+        raise Exception("Invalid input")
+
 
 
 def remap_interval(val,
@@ -84,8 +158,10 @@ def remap_interval(val,
         >>> remap_interval(5, 4, 6, 1, 2)
         1.5
     """
-    # TODO: implement this
-    pass
+    input_width = input_interval_end - input_interval_start
+    output_width = output_interval_end - output_interval_start
+    amt_into_interval = (val - input_interval_start)/input_width
+    return output_interval_start + output_width*amt_into_interval
 
 
 def color_map(val):
@@ -107,30 +183,8 @@ def color_map(val):
         >>> color_map(0.5)
         191
     """
-    # NOTE: This relies on remap_interval, which you must provide
     color_code = remap_interval(val, -1, 1, 0, 255)
     return int(color_code)
-
-
-def test_image(filename, x_size=350, y_size=350):
-    """Generate a test image with random pixels and save as an image file.
-
-    Args:
-        filename: string filename for image (should be .png)
-        x_size, y_size: optional args to set image dimensions (default: 350)
-    """
-    # Create image and loop over all pixels
-    im = Image.new("RGB", (x_size, y_size))
-    pixels = im.load()
-    for i in range(x_size):
-        for j in range(y_size):
-            x = remap_interval(i, 0, x_size, -1, 1)
-            y = remap_interval(j, 0, y_size, -1, 1)
-            pixels[i, j] = (random.randint(0, 255),  # Red channel
-                            random.randint(0, 255),  # Green channel
-                            random.randint(0, 255))  # Blue channel
-
-    im.save(filename)
 
 
 def generate_art(filename, x_size=350, y_size=350):
@@ -140,12 +194,12 @@ def generate_art(filename, x_size=350, y_size=350):
         filename: string filename for image (should be .png)
         x_size, y_size: optional args to set image dimensions (default: 350)
     """
-    # Functions for red, green, and blue channels - where the magic happens!
-    red_function = ["x"]
-    green_function = ["y"]
-    blue_function = ["x"]
-
     # Create image and loop over all pixels
+
+    r_f = random_f(6,8)
+    g_f = random_f(6,8)
+    b_f = random_f(6,8)
+
     im = Image.new("RGB", (x_size, y_size))
     pixels = im.load()
     for i in range(x_size):
@@ -153,23 +207,18 @@ def generate_art(filename, x_size=350, y_size=350):
             x = remap_interval(i, 0, x_size, -1, 1)
             y = remap_interval(j, 0, y_size, -1, 1)
             pixels[i, j] = (
-                color_map(evaluate_random_function(red_function, x, y)),
-                color_map(evaluate_random_function(green_function, x, y)),
-                color_map(evaluate_random_function(blue_function, x, y))
+                color_map(eval_f(r_f,x,y)),
+                color_map(eval_f(g_f,x,y)),
+                color_map(eval_f(b_f,x,y)),
             )
 
     im.save(filename)
+    im.show()
 
 
 if __name__ == '__main__':
     import doctest
-    doctest.testmod()
-
-    # Create some computational art!
-    # TODO: Un-comment the generate_art function call after you
-    #       implement remap_interval and evaluate_random_function
-    # generate_art("myart.png")
-
-    # Test that PIL is installed correctly
-    # TODO: Comment or remove this function call after testing PIL install
-    test_image("noise.png")
+    # doctest.testmod()
+    # doctest.run_docstring_examples(random_f_lambda, globals(), verbose = True)
+    # print(random_f_lambda(6,8,.5,.7))
+    generate_art("myart8.png")
